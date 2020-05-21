121853f4523e0da03b0b5412bb2bd2573f5e46ee
Miracle Gabriel <sixpathdev@gmailc.com>
2020-05-21 10:11:43 +0100

    UI done, UX be done using a server-side language

---
+++ index.js
@@ -0,0 +1,6 @@
+$(".timeframe_data").on("click", function(){
+    // alert(window.location.pathname)
+    $(this).replaceWith('<div class="timeframe_data_confirm"><div>9:00</div><div>confirm</div></div>')
+})
+
+
